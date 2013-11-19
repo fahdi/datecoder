c61104c624afb9b56f427f4e4159a755a07f505a
Fahad Murtaza <fahdim@gmail.com>
2013-11-20 03:32:24 +0500

    Javascript, HTML and CSS updates
    Added new versions of jQuery and also a minified version for the same
    Wrote some test code to update JSON data in a dropdown. This code will
    be used in the datecoder form to update the drop down fields with new
    options.
    Created two gists for the generic JSON with and without AJAX
    https://gist.github.com/fahdi/7553465 and
    https://gist.github.com/fahdi/7553327
    Added divider code for the dropdowns.
    
    Note: Next version will contain the new code in datecoder.js file which
    will use JSON data to update the respective format drop down fields on
    change of an element dropdown. Excited!

--- js/datecoder.js
+++ js/datecoder.js
@@ -1,10 +1,14 @@
-$( document ).ready(function() {
+//$( document ).ready(function() {
   // Handler for .ready() called.
 
-$("#elements").on("change", function(e) {
+//$("#elements").on("change", function(e) {
 	
 	//alert('it works');
-    $('#formats').find('select').val($(this).val());
+//	foo = $(this);
+	
+
+//	console.log(foo.text());
+    //$('#formats').find('select').val($(this).val());
 
     // One strategy
     //$("#format1, #format2, #format3, #format4, #format5").val($(this).val());
@@ -14,7 +18,15 @@ $("#elements").on("change", function(e) {
     //var value = $(this).val();
     //$('select[name=myddl] option[value='+value+']').attr('selected', 'selected');
 
-});
+//});
+//});
 
 
+$(document).ready(function () {
+   $("#element1").on("change", function(e) {
+        $('#txtID').val($(this).val());
+        $('#txtOption').val($('#element1 option:selected').text());
+       // $('#selItem').val($(this).val());
+       // $('#txtItem').val($('#selItem option:selected').text());
+    }); 
 });
